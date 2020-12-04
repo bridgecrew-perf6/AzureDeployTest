af2ad8d32f56cb11c9d0c531243b9178f0001295
matheus siqueira <matheus2332@gmail.com>
2020-12-03 22:16:09 -0300

    add module mongodb add domain add services

---
+++ Modules/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using AzureDeployTest.Repository;
+using AzureDeployTest.Repository.Repository;
+using AzureDeployTest.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzureDeployTest.Modules
+{
+    public static class DependencyInjection
+    {
+        public static void AddDependencyInjection(this IServiceCollection service)
+        {
+            service.AddSingleton(typeof(IProdutoRepository), typeof(ProdutoRepository));
+            service.AddTransient(typeof(IProdutoService), typeof(ProdutoService));
+        }
+    }
+}
